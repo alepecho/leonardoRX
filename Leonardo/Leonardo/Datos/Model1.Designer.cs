068fc84f76fd0f02001221189c72d8cfb99a2514
Alejandro Ramírez Astúa <aramiras@ccss.sa.cr>
2018-08-29 07:50:58 -0600

    Prueba de mensaje ASG

---
+++ Leonardo/Leonardo/Datos/Model1.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\aramiras\Documents\Visual Studio 2012\Projects\Prueba_Leonardo\Leonardo\Leonardo\Datos\Model1.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
---
+++ Leonardo/Leonardo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Leonardo.MvcApplication" Language="C#" %>
