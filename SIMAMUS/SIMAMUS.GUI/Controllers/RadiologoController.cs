6b25b58cd482faac068d6df9d1bdb2ae2ddbcaaa
lbrenes <lbrenes1098@gmail.com>
2018-09-21 08:14:32 -0600

    merge realizado :v

--- SIMAMUS/SIMAMUS.GUI/Controllers/RadiologoController.cs
+++ SIMAMUS/SIMAMUS.GUI/Controllers/RadiologoController.cs
@@ -64,9 +64,8 @@ namespace SIMAMUS.GUI.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "1")]
-        public ActionResult Create([Bind(Include = "CodigoRadiologo,NombreUsuario,Activo")] Radiologo radiologo)
+        public ActionResult Create([Bind(Include = "CodigoRadiologo,NombreUsuario")] Radiologo radiologo)
         {
-            radiologo.Activo = true;
             if (ModelState.IsValid)
             {
                 db.Radiologo.Add(radiologo);
@@ -101,7 +100,7 @@ namespace SIMAMUS.GUI.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "1")]
-        public ActionResult Edit([Bind(Include = "CodigoRadiologo,NombreUsuario,Activo")] Radiologo radiologo)
+        public ActionResult Edit([Bind(Include = "CodigoRadiologo,NombreUsuario")] Radiologo radiologo)
         {
             if (ModelState.IsValid)
             {
@@ -183,9 +182,8 @@ namespace SIMAMUS.GUI.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "1,2")]
-        public ActionResult CreateAdministrador([Bind(Include = "CodigoRadiologo,NombreUsuario,Activo")] Radiologo radiologo)
+        public ActionResult CreateAdministrador([Bind(Include = "CodigoRadiologo,NombreUsuario")] Radiologo radiologo)
         {
-            radiologo.Activo = true;
             if (ModelState.IsValid)
             {
                 db.Radiologo.Add(radiologo);
@@ -220,7 +218,7 @@ namespace SIMAMUS.GUI.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "1,2")]
-        public ActionResult EditAdministrador([Bind(Include = "CodigoRadiologo,NombreUsuario,Activo")] Radiologo radiologo)
+        public ActionResult EditAdministrador([Bind(Include = "CodigoRadiologo,NombreUsuario")] Radiologo radiologo)
         {
             if (ModelState.IsValid)
             {
@@ -302,9 +300,8 @@ namespace SIMAMUS.GUI.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "1,3")]
-        public ActionResult CreateTecnico([Bind(Include = "CodigoRadiologo,NombreUsuario,Activo")] Radiologo radiologo)
+        public ActionResult CreateTecnico([Bind(Include = "CodigoRadiologo,NombreUsuario")] Radiologo radiologo)
         {
-            radiologo.Activo = true;
             if (ModelState.IsValid)
             {
                 db.Radiologo.Add(radiologo);
@@ -339,7 +336,7 @@ namespace SIMAMUS.GUI.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "1,3")]
-        public ActionResult EditTecnico([Bind(Include = "CodigoRadiologo,NombreUsuario,Activo")] Radiologo radiologo)
+        public ActionResult EditTecnico([Bind(Include = "CodigoRadiologo,NombreUsuario")] Radiologo radiologo)
         {
             if (ModelState.IsValid)
             {
